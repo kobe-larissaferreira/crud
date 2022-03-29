79dbdad279fb149a715e07dfd901e0d148dddc12
Larissa Ferreira <larissa.ferreira@kobe.io>
2022-03-29 11:51:28 -0300

    feat: incluindo rotas

--- src/routes.ts
+++ src/routes.ts
@@ -8,4 +8,12 @@ const usersController = new UsersController();
 
 routes.post("/users", usersController.create);
 
+routes.get("/users", usersController.index);
+
+routes.get("/users/:id", usersController.show);
+
+routes.delete("/users/:id", usersController.delete);
+
+routes.put("/users/:id", usersController.update);
+
 export { routes };
