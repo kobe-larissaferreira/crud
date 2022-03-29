ea042eda1fce9f9040827b5ef309579e44c445cf
Larissa Ferreira <larissa.ferreira@kobe.io>
2022-03-29 11:51:15 -0300

    feat: incluindo metodos para o crud

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -6,10 +6,62 @@ class UsersController {
     const { usuario, phone, email } = request.body;
 
     const usersServices = new UsersServices();
+    try {
+      const users = await usersServices.create({ usuario, phone, email });
+      return response.json(users);
+    } catch (err) {
+      return response.status(404).json({ message: err.message });
+    }
+  }
 
-    const users = await usersServices.create({ usuario, phone, email });
+  async index(request: Request, response: Response) {
+    const usersServices = new UsersServices();
 
-    return response.json(users);
+    try {
+      const users = await usersServices.index();
+      return response.json(users);
+    } catch (err) {
+      return response.status(404).json({ message: err.message });
+    }
+  }
+
+  async show(request: Request, response: Response) {
+    const usersServices = new UsersServices();
+    //id parametro da rota = request.params.id
+    const { id } = request.params;
+
+    try {
+      const users = await usersServices.show({ id });
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ message: err.message });
+    }
+  }
+  async delete(req: Request, res: Response) {
+    const usersServices = new UsersServices();
+
+    const { id } = req.params;
+
+    try {
+      await usersServices.delete({ id });
+      return res.json({ message: "User deleted successfully" });
+    } catch (err) {
+      return res.status(400).json({ message: err.message });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const usersServices = new UsersServices();
+
+    const { id } = req.params;
+    const { usuario, phone, email } = req.body;
+
+    try {
+      const users = await usersServices.update({ id, usuario, phone, email });
+      return res.json({ message: "User updated successfully" });
+    } catch (err) {
+      return res.status(400).json({ message: err.message });
+    }
   }
 }
 
--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -7,11 +7,76 @@ interface IUserCreate {
   email: string;
 }
 
+interface IUsersId {
+  id: string;
+}
+
+interface IUsersUpdate {
+  id: string;
+  usuario: string;
+  phone: string;
+  email: string;
+}
+
 class UsersServices {
   async create({ usuario, phone, email }: IUserCreate) {
     const usersRepository = getCustomRepository(UsersRepository);
+
+    const emailAlreadyExists = await usersRepository.findOne({ email });
+    if (emailAlreadyExists) {
+      throw new Error("Email already exists!");
+    }
     const users = usersRepository.create({ usuario, phone, email });
     await usersRepository.save(users);
+
+    return users;
+  }
+
+  async index() {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const users = await usersRepository.find();
+
+    return users;
+  }
+
+  async show({ id }: IUsersId) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const users = await usersRepository.findOne({ id });
+
+    if (!users) {
+      throw new Error("User id not found");
+    }
+
+    return users;
+  }
+
+  async delete({ id }: IUsersId) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const users = await usersRepository.findOne({ id });
+    if (!users) {
+      throw new Error("User id not found");
+    }
+    return await usersRepository.delete({ id });
+  }
+
+  async update({ id, usuario, phone, email }: IUsersUpdate) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    let users = await usersRepository.findOne({ id });
+
+    if (!users) {
+      throw new Error("User id not found");
+    }
+    await usersRepository.update(id, {
+      usuario,
+      phone,
+      email,
+    });
+    await usersRepository.findOne({ id });
+
     return users;
   }
 }
